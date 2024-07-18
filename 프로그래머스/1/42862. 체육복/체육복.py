1796590434a022cee438e40eaad12a686558ab4d
hjoo830 <105642658+hjoo830@users.noreply.github.com>
2024-07-18 21:02:45 +0900

    [level 1] Title: 체육복, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,9 +1,8 @@
 def solution(n, lost, reserve):
     answer = 0 
     other=[]
-    
     choice=[]
-    
+
     lost.sort()
     reserve.sort()
     
@@ -14,17 +13,12 @@ def solution(n, lost, reserve):
             reserve.remove(i)
         elif(i not in lost and i not in reserve):
             other.append(i)
-
-    print("other",other)
-    print("lost",lost)
-    print("reserve",reserve)
     
     can=[[0 for j in range(2)] for i in range(len(reserve))]
     for i in range(len(reserve)):
         can[i][0]=reserve[i]-1
         can[i][1]=reserve[i]+1
-    print("can",can)
-    
+
     for i in range(len(reserve)-1):
         if (can[i][0] not in choice and can[i][0]>0 and can[i][0] not in other and can[i][0] not in reserve):
             choice.append(can[i][0])
@@ -39,8 +33,6 @@ def solution(n, lost, reserve):
             choice.append(reserve[0]-1)
         elif (reserve[0]+1<=n and reserve[0]+1 in lost):
             choice.append(reserve[0]+1)
-            
-    print("choice",choice)
+
     answer=len(choice)+len(other)+len(reserve)
-    
     return answer
