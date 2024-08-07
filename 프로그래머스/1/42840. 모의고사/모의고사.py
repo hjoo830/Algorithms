f221eee8196012ddd92ee82e0792feadcd1892b1
hjoo830 <105642658+hjoo830@users.noreply.github.com>
2024-08-08 00:12:21 +0900

    [level 1] Title: 모의고사, Time: 3.63 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,21 @@
+def solution(answers):
+    answer = []
+    p1=[1, 2, 3, 4, 5]
+    p2=[2, 1, 2, 3, 2, 4, 2, 5]
+    p3=[3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    a1=a2=a3=0
+    
+    for i in range(len(answers)):
+        if answers[i]==p1[i%len(p1)]:
+            a1+=1
+        if answers[i]==p2[i%len(p2)]:
+            a2+=1
+        if answers[i]==p3[i%len(p3)]:
+            a3+=1
+    if a1==max(a1,a2,a3):
+        answer.append(1)
+    if a2==max(a1,a2,a3):
+        answer.append(2)
+    if a3==max(a1,a2,a3):
+        answer.append(3)
+    return answer
