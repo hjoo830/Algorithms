67dbcf53f5b452d5b6e1b4964992dbb7e82739f3
hjoo830 <105642658+hjoo830@users.noreply.github.com>
2024-08-12 00:59:56 +0900

    [level 0] Title: 7의 개수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.py
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.py
@@ -1,6 +1,5 @@
 def solution(array):
     answer = 0
-    for i in array:
-        for j in str(i):
-            if j=='7': answer+=1
+    for n in str(array):
+        if n=='7': answer+=1
     return answer
