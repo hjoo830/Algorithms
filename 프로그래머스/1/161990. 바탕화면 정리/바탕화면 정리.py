3ab1feba7b42f4adb8aedf0fba899dc29a515b06
hjoo830 <105642658+hjoo830@users.noreply.github.com>
2024-10-02 11:49:57 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.60 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,23 +1,12 @@
 def solution(wallpaper):
     grid = list(map(list, wallpaper))
-    
-    minRow = len(wallpaper)
-    minCol = len(wallpaper[0])
-    maxRow = 0
-    maxCol = 0
+    r = []
+    c = []
     
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == '#':
-                row = i 
-                col = j
-                if minRow > i:
-                    minRow = i
-                if minCol > j:
-                    minCol = j
-                if maxRow < i:
-                    maxRow = i
-                if maxCol < j:
-                    maxCol = j
+                r.append(i) 
+                c.append(j)
                     
-    return [minRow, minCol, maxRow + 1, maxCol + 1]
+    return [min(r), min(c), max(r) + 1, max(c) + 1]
