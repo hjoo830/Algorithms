6718c2b544143dc9c1bb7e50ef41685cf85ab0fc
hjoo830 <105642658+hjoo830@users.noreply.github.com>
2024-10-29 10:17:48 +0900

    [level 2] Title: 숫자의 표현, Time: 1.95 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,18 @@
+def solution(n):
+    answer = 0
+    start, end = 1, 1
+    current_sum = 1
+
+    while start <= n:
+        if current_sum == n:
+            answer += 1
+            current_sum -= start
+            start += 1
+        elif current_sum < n:
+            end += 1
+            current_sum += end
+        else:
+            current_sum -= start
+            start += 1
+
+    return answer
