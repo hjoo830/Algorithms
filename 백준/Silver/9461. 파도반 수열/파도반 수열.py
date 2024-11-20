31e31d8a0ef1f6160df64a25be6af77b34f141b1
hjoo830 <105642658+hjoo830@users.noreply.github.com>
2024-11-20 10:15:19 +0900

    [Silver III] Title: 파도반 수열, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+t = int(input())
+for tc in range(t):
+  n = int(input())
+  dp = [0] * (n + 1)
+
+  if n >= 1:
+    dp[1] = 1
+  if n >= 2:
+    dp[2] = 1
+  if n >= 3:
+    dp[3] = 1
+  
+  for i in range(4, n + 1):
+    dp[i] = dp[i - 3] + dp[i - 2]
+  print(dp[n])
