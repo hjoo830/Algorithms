fd90d8d18e55e224ad2d72438387b68b8cd7257b
hjoo830 <105642658+hjoo830@users.noreply.github.com>
2025-04-24 00:56:30 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 2612 ms, Memory: 42656 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,11 @@
+n = int(input())
+company = set()
+for i in range(n):
+  name, record = input().split()
+  if record == 'enter':
+    company.add(name)
+  elif name in company:
+    company.discard(name)
+
+for person in sorted(company, reverse=True):
+   print(person)
