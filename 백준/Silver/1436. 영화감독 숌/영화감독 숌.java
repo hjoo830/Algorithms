173095e5883346bbf70695c8b34bb957a92fd7f1
hjoo830 <105642658+hjoo830@users.noreply.github.com>
2025-08-05 16:46:15 +0900

    [Silver V] Title: 영화감독 숌, Time: 276 ms, Memory: 84820 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int num = 665;
+        int cnt = 0;
+        while(cnt < n){
+            num++;
+            if ((num+"").contains("666")){
+                cnt++;
+            }
+        }
+        System.out.println(num);
+        
+        br.close();
+    }
+}
