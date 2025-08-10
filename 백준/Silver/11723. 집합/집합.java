64b7ce5b1b3364b56e09aea34a858c88e7715dc8
hjoo830 <105642658+hjoo830@users.noreply.github.com>
2025-08-10 18:23:13 +0900

    [Silver V] Title: 집합, Time: 1036 ms, Memory: 314368 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -2,56 +2,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringBuilder sb = new StringBuilder();
-    	int m = Integer.parseInt(br.readLine());
-    	Set<Integer> s = new HashSet<>();
-    	
-    	for(int i = 0; i < m; i++) {
-        	StringTokenizer st = new StringTokenizer(br.readLine());
-        	
-        	String c = st.nextToken();
-        	
-        	switch (c) {
-				case "all": {
-					s.clear();
-					for (int j = 1; j <= 20; j++) {
-						s.add(j);
-					}
-					break;
-				}
-				case "empty": {
-					s.clear();
-					break;	
-				}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int m = Integer.parseInt(br.readLine());
+		int s = 0;
+		
+		for(int i = 0; i < m; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String c = st.nextToken();
+			
+			switch (c) {
 				case "add": {
 					int x = Integer.parseInt(st.nextToken());
-					s.add(x);
-					break;	
+					s = s | (1 << x);
+					break;
 				}
 				case "remove": {
 					int x = Integer.parseInt(st.nextToken());
-					if (s.contains(x)) {
-						s.remove(x);
-					}
-					break;	
+					s = s & ~(1 << x);
+					break;
 				}
 				case "check": {
 					int x = Integer.parseInt(st.nextToken());
-					sb.append(s.contains(x) ? "1\n" : "0\n");
-					break;	
-				}
+					
+					sb.append((s & (1 << x)) != 0 ? "1\n" : "0\n");
+					break;
+				}			
 				case "toggle": {
 					int x = Integer.parseInt(st.nextToken());
-					if (s.contains(x)) {
-						s.remove(x);
-					}else {
-						s.add(x);
-					}
+					s = s ^ (1 << x);
+					break;
+				}	
+				case "all": {
+					s = (1 << 21) - 1;
+					break;
 				}
-        	}  	
-    	}
-    	System.out.println(sb);
-    }
+				case "empty": {
+					s = 0;
+					break;
+				}
+			}
+		}
+		System.out.println(sb);
+	}
 }
