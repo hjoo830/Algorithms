51b2e8bece83656654765924c8ae4ebf56682ce9
hjoo830 <105642658+hjoo830@users.noreply.github.com>
2025-08-14 16:59:55 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,22 +1,30 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int order = scanner.nextInt();
-        int num = 0;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 
-        while (true) {
-            if ( order%5 == 0 ) {
-                System.out.println( order/5 + num );
-                break;
-            }
-            else if ( order < 0 ){
-                System.out.println("-1");
-                break;
-            }
-            order -= 3;
-            num++;
-        }
-    }
+		int[] dp = new int[n + 1];
+		Arrays.fill(dp, 5000);
+		
+		if (n >= 3) {
+			dp[3] = 1;			
+		}
+		if (n >= 5) {
+			dp[5] = 1;
+		}
+		
+		for(int i = 6; i < n + 1; i++) {
+			dp[i] = Math.min(dp[i-3], dp[i-5]) + 1;
+		}
+		
+		if(dp[n] >= 5000) {
+			System.out.println(-1);
+		} else {
+			System.out.println(dp[n]);
+		}		
+	}
 }
