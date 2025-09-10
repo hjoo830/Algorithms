49bd173e18d41128645e81c2dc8a5438487f96eb
hjoo830 <105642658+hjoo830@users.noreply.github.com>
2025-09-10 14:29:54 +0900

    [Gold IV] Title: 키 순서, Time: 628 ms, Memory: 36556 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -2,67 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int n, sCnt, tCnt, answer;
-	static List<List<Integer>> smaller = new ArrayList<>();
-	static List<List<Integer>> taller = new ArrayList<>();
-	static boolean[] vS, vT;
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-		
-		for (int i = 0; i <= n; i++) {
-			smaller.add(new ArrayList<>());
-			taller.add(new ArrayList<>());
-		}
-		
+
+		boolean[][] adj = new boolean[n + 1][n + 1];
+
 		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int t = Integer.parseInt(st.nextToken());
-			
-			taller.get(s).add(t);
-			smaller.get(t).add(s);
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			adj[a][b] = true; // a < b
 		}
-		
+
+		for (int k = 1; k <= n; k++) {
+			for (int i = 1; i <= n; i++) {
+				for (int j = 1; j <= n; j++) {
+					if (adj[i][k] && adj[k][j]) adj[i][j] = true;
+				}
+			}				
+		}
+
+		int answer = 0;
 		for (int i = 1; i <= n; i++) {
-			sCnt = 0;
-			tCnt = 0;
-			vS = new boolean[n + 1];
-			vT = new boolean[n + 1];
-			
-			dfsS(i);
-			dfsT(i);
-			
-			if (sCnt + tCnt == n - 1) {
-				answer++;
+			int count = 0;
+			for (int j = 1; j <= n; j++) {
+				if (adj[i][j] || adj[j][i]) count++;
 			}
-		}
-		
-		System.out.println(answer);		
+			if (count == n - 1) answer++;
+		}				
+
+		System.out.println(answer);
 	}
-	
-	static void dfsS(int idx) {
-		vS[idx] = true;
-		
-		for (int s : smaller.get(idx)) {
-			if (!vS[s]) {
-				sCnt++;
-				dfsS(s);				
-			}
-		}
-    }
-	
-	static void dfsT(int idx) {
-		vT[idx] = true;
-		
-		for (int t : taller.get(idx)) {
-			if (!vT[t]) {
-				tCnt++;
-				dfsT(t);				
-			}
-		}
-    }
 }
