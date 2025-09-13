8faa682da6b14b65a0bcc80f17375009d18b7ece
hjoo830 <105642658+hjoo830@users.noreply.github.com>
2025-09-13 20:23:29 +0900

    [D3] Title: 0/1 Knapsack, Time: 100 ms, Memory: 28,564 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
@@ -1,8 +1,8 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
-	public static void main(String[] args) throws Exception {
+public class Solution {	
+	public static void main(String[] args) throws IOException {		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int t = Integer.parseInt(br.readLine());
@@ -11,25 +11,26 @@ public class Solution {
 			int n = Integer.parseInt(st.nextToken());
 			int k = Integer.parseInt(st.nextToken());
 			
-			int[] w = new int[n];
-			int[] v = new int[n];
+			int[] v = new int[n]; // 부피
+			int[] c = new int[n]; // 가치
 			
 			for (int i = 0; i < n; i++) {
 				st = new StringTokenizer(br.readLine());
-				w[i] = Integer.parseInt(st.nextToken());
 				v[i] = Integer.parseInt(st.nextToken());
+				c[i] = Integer.parseInt(st.nextToken());
 			}
 			
-			int[] dp = new int[k + 1];
-			for (int i = 0; i < n; i++) {
-				for (int j = k; j >= w[i]; j--) {
-					dp[j] = Math.max(dp[j], dp[j - w[i]]+ v[i]);				
+			int[][] dp = new int[n + 1][k + 1];
+			
+			for (int i = 1; i <= n; i++) {
+				for (int j = 1; j <= k; j++) {
+					if (v[i - 1] > j) dp[i][j] = dp[i - 1][j];
+					else dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - v[i - 1]] + c[i - 1]);
 				}
 			}
 			
-			sb.append("#").append(tc).append(" ").append(dp[k]).append("\n");
+			sb.append("#").append(tc).append(" ").append(dp[n][k]).append("\n");
 		}
-		
 		System.out.println(sb);
 	}
 }
