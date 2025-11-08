84f345723f353b709c26ad88de4cba22f0a4cc77
hjoo830 <105642658+hjoo830@users.noreply.github.com>
2025-11-08 22:56:56 +0900

    [Bronze IV] Title: R2, Time: 100 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int r1 = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+
+        System.out.println(2 * s - r1);
+    }
+}
