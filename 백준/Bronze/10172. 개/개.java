5b4ab2b94c2fa71304a5594c4cf90b65b9df50cb
황효주 <105642658+hjoo830@users.noreply.github.com>
2025-11-21 12:54:49 +0900

    [Bronze V] Title: 개, Time: 100 ms, Memory: 13956 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		System.out.println("|\\_/|\n"
+				+ "|q p|   /}\n"
+				+ "( 0 )\"\"\"\\\n"
+				+ "|\"^\"`    |\n"
+				+ "||_/=\\\\__|");
+	}
+}
