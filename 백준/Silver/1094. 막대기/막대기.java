0b66c5d169f2ed528035584c53e26e410e95155f
황효주 <hjoo830@naver.com>
2025-12-26 00:49:00 +0900

    [Silver V] Title: 막대기, Time: 108 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int x = Integer.parseInt(st.nextToken());
+		int stick = 64;
+		int temp = 0;
+		int count = 0;
+		int result = x;
+
+		while (true) {
+			if (x == 64) {
+				count++;
+				break;
+			}
+
+			stick /= 2; 
+			if (stick <= x) {
+				temp += stick; 
+				count++;
+				if (result == temp) break;
+				x -= stick;
+			}
+		}
+		System.out.println(count);
+	}
+}
