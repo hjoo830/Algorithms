2bbac9804bff4d217dcac6f2c7dbce72a8f9144f
황효주 <hjoo830@naver.com>
2026-01-06 23:01:28 +0900

    [Bronze V] Title: 오늘 날짜, Time: 168 ms, Memory: 17160 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        LocalDate today = LocalDate.now();
+        System.out.println(today);
+    }
+}
