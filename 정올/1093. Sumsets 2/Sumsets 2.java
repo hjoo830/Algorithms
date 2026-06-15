e425dd400ed2467efe8d46cd6f208e259e956abd
황효주 <hjoo830@naver.com>
2026-06-15 18:37:52 +0900

    [Gold V] Title: Sumsets 2, Time: 230 ms, Memory: 39.5 MB

---
+++ 정올/1093. Sumsets 2/Sumsets 2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[n + 1];
+        dp[0] = 1;
+
+        for (int coin = 1; coin <= n; coin *= 2) {
+            for (int i = coin; i <= n; i++) {
+                dp[i] = (dp[i] + dp[i - coin]) % 1000000007;
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
