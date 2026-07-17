43375a7633a1fe9ebc56c5ea310a552efd22149c
황효주 <hjoo830@naver.com>
2026-07-17 23:32:38 +0900

    [Bronze IV] Title: 최대값, Time: 177 ms, Memory: 33.0 MB

---
+++ 정올/1035. 최대값/최대값.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int max = 0;
+        int idx = 0;
+        for (int i = 0; i < 9; i++) {
+            int n = Integer.parseInt(br.readLine());
+            if (n > max) {
+                max = n;
+                idx = i;
+            }
+        }
+        System.out.println(max);
+        System.out.println(idx + 1);
+    }
+}
